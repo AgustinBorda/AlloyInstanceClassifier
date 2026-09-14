5a4d8818c7108294c11813dfa890d32837fd6dd2
Agustin Borda <aborda@dc.exa.unrc.edu.ar>
2026-09-14 20:09:33 -0300

    progress
    Signed-off-by: Agustin Borda <aborda@dc.exa.unrc.edu.ar>

--- src/alloySpecClassifier/SimpleAlloySpecClassifier.java
+++ src/alloySpecClassifier/SimpleAlloySpecClassifier.java
@@ -49,7 +49,7 @@ public class SimpleAlloySpecClassifier implements AlloySpecClassifier {
             if (correctModel.hasFuns()) {
                 instances += runner.runModelAndReturnInstanceQuantity(funGenerator.generateModelForClassification(correctModel, buggyModel, scope));
             }
-            System.out.println("The model " + child.getName() + (instances == 10000 ? " has >=": " has ") + instances + " bug-revealing instances.");
+            System.out.println((models+1) + "/" + Objects.requireNonNull(dir.listFiles()).length + " The model " + child.getName() + (instances == 10000 ? " has >=": " has ") + instances + " bug-revealing instances.");
             totalInstances += instances;
             models++;
             if(instances < 10000)
